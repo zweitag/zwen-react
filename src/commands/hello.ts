8b7eab17ce5d6f9839db932192b20f3f2bdf5b89
Johannes Hollekamp <johannes.hollekamp@zweitag.de>
2018-04-30 16:39:30 +0200

    add a test yeoman generator and link it with oclif

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,31 +1,12 @@
-import {Command, flags} from '@oclif/command'
+import {Command} from '@oclif/command'
+import {createEnv} from 'yeoman-environment'
 
 export default class Hello extends Command {
   static description = 'describe the command here'
 
-  static examples = [
-    `$ zwen hello
-hello world from ./src/hello.ts!
-`,
-  ]
-
-  static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(Hello)
-
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/commands/hello.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    const env = createEnv()
+    env.register(require.resolve('../../templates/generators/app'), 'zwen:app')
+    env.run('zwen:app', () => {})
   }
 }
