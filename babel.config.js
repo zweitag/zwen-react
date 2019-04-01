b6c9641a6a3ab6c0b566e37a81df5e44e0bcd90f
Alex Kott <tetlokax@gmail.com>
2019-04-01 12:24:15 -0600

    update dependencies, include babel

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    ['@babel/env', {
+      targets: { node: true },
+    }],
+    '@babel/typescript',
+  ],
+  plugins: [
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+  ],
+};
